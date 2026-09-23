874b86abee2b4560ff559f65eb091bb3ccd4f1de
Alisson Albuquerque <alissonalbuquerque.dev@gmail.com>
2026-09-23 12:09:47 -0300

    insert new line in EOF

--- src/orion/exceptions/config.lua
+++ src/orion/exceptions/config.lua
@@ -1,9 +1,8 @@
 local __config__ = {}
 
     __config__.__namespace = 'orion.exceptions'
-    __config__.__modules   = {'TypeError', 'ValueError', 'TryCatchException'}
+    __config__.__modules   = {'TryCatchException', 'TypeError', 'ValueError'}
     __config__.__package   = require('orion.core.Package')
     __config__.__class     = require('orion.core.Class')
 
 return __config__
-
